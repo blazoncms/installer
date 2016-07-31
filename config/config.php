7bb0f8021844bc5e030e5ddde5376e91f9aebbe9
Westin Shafer <westin@havenly.com>
2016-07-31 09:55:33 -0600

    Reset installer

--- config/config.php
+++ config/config.php
@@ -1,24 +1,4 @@
 <?php
 
 return [
-    'dependencies' => [
-        'factories' => [
-            \BlazonCms\Installer\Action\Install::class => \BlazonCms\Installer\Action\InstallFactory::class
-        ],
-    ],
-
-    'routes' => array(
-        'BlazonCms\Install' => array(
-            'name' => 'install',
-            'path' => '/install',
-            'middleware' => \BlazonCms\Installer\Action\Install::class,
-            'allowed_methods' => [ 'GET' ],
-        ),
-    ),
-
-    'templates' => [
-        'paths' => [
-            'installer'    => [__DIR__.'/../templates']
-        ],
-    ],
 ];
